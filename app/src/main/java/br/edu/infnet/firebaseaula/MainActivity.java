e547587ddfcb3696650d25ad5289ffbddf3f188e
lucas-soares <lucas3designer@gmail.com>
2017-11-24 20:54:55 -0200

    # [IMPL] - Funcionalidade "Upload de Imagem" implementada

--- app/src/main/java/br/edu/infnet/firebaseaula/MainActivity.java
+++ app/src/main/java/br/edu/infnet/firebaseaula/MainActivity.java
@@ -1,13 +1,88 @@
 package br.edu.infnet.firebaseaula;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnUploadImagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnUploadImagem = (Button) findViewById(R.id.btnUploadImagem);
+
+        /**
+         * Obtem a referencia ao objeto FirebaseStorage ondem os arquivos são armazenados.
+         */
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        final StorageReference storageReference = storage.getReferenceFromUrl("gs://fir-aula-a14f8.appspot.com/").child("firebase.png");
+
+        /**
+         * Realiza o UPLOAD DE IMAGEM
+          */
+
+        btnUploadImagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fazerUpload(storageReference);
+            }
+        });
+
+    }
+
+    private void fazerUpload(StorageReference storageReference){
+        AssetManager assetManager = MainActivity.this.getAssets();
+        InputStream istr;
+        Bitmap bitmap;
+        try {
+            //recupera a imagem na pasta de assets
+            istr = assetManager.open("firebase.png");
+            bitmap = BitmapFactory.decodeStream(istr);
+
+            //decodifica em bytes
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            byte[] data = outputStream.toByteArray();
+
+            //carrega no firebase                    showProgressDialog("Upload Bitmap", "Uploading...");
+            UploadTask uploadTask = storageReference.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    exception.printStackTrace();
+                    //dismissProgressDialog();
+                    Toast.makeText(MainActivity.this, "Upload Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                   // dismissProgressDialog();
+                    Toast.makeText(MainActivity.this, "Upload successful!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
